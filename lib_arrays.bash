1c362d490972036a0eaf51c919f0cdc6f4035bc7
Robin Hellmers <robin@hellmers.se>
2024-01-10 19:45:12 +0100

    Add lib_arrays.bash

---
+++ lib_arrays.bash
@@ -0,0 +1,49 @@
+#####################
+### Guard library ###
+#####################
+guard_source_max_once() {
+    local file_name="$(basename "${BASH_SOURCE[0]}")"
+    local guard_var="guard_${file_name%.*}" # file_name wo file extension
+
+    [[ "${!guard_var}" ]] && return 1
+    [[ "$guard_var" =~ ^[_a-zA-Z][_a-zA-Z0-9]*$ ]] \
+        || { echo "Invalid guard: '$guard_var'"; exit 1; }
+    declare -gr "$guard_var=true"
+}
+
+guard_source_max_once || return 0
+
+##############################
+### Library initialization ###
+##############################
+init_lib()
+{
+    # Unset as only called once and most likely overwritten when sourcing libs
+    unset -f init_lib
+
+    if ! [[ -d "$LIB_PATH" ]]
+    then
+        echo "LIB_PATH is not defined to a directory for the sourced script."
+        echo "LIB_PATH: '$LIB_PATH'"
+        exit 1
+    fi
+
+    ### Source libraries ###
+    #
+    # Always start sourcing 'lib_core.bash' with the command
+    # source "$LIB_PATH/lib_core.bash" || exit 1
+    # Libraries thereafter should be sourced with the command
+    # source_lib "$LIB_PATH/<lib_name>"
+    source "$LIB_PATH/lib_core.bash" || exit 1
+}
+
+init_lib
+
+#####################
+### Library start ###
+#####################
+
+###
+# List of functions for usage outside of lib
+#
+###
