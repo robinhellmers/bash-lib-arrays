391ac1c4aa391bf3acafcc75395cf290893390c8
Robin Hellmers <robin@hellmers.se>
2024-01-11 07:32:13 +0100

    Add find_array_index_by_value()

--- lib_arrays.bash
+++ lib_arrays.bash
@@ -35,6 +35,8 @@ init_lib()
     # Libraries thereafter should be sourced with the command
     # source_lib "$LIB_PATH/<lib_name>"
     source "$LIB_PATH/lib_core.bash" || exit 1
+    source_lib "$LIB_PATH/lib_handle_input.bash"
+    source_lib "$LIB_PATH/lib_dynamic_variables.bash"
 }
 
 init_lib
@@ -46,4 +48,68 @@ init_lib
 ###
 # List of functions for usage outside of lib
 #
+# - find_array_index_by_value()
 ###
+
+register_help_text 'find_array_index_by_value' \
+'find_array_index_by_value <value> <array_len> <array>
+
+Find the first index of the array, which stores the given value.
+
+Input:
+<value>: The value to find in the array
+<array_len>: The length of the array ( ${#array[@]} )
+<array>: The array values ( ${array[@]} )
+
+Output:
+Variable <found_index>: Index of first array element containing <value>
+
+Return value:
+0 if found
+1 if not found'
+
+# No flags
+register_function_flags 'find_array_index_by_value' \
+                        '-v' '--verbose' 'false' 'Verbose output'
+
+find_array_index_by_value()
+{
+    local input_value
+    local input_array=()
+    _handle_args_find_array_index_by_value "$@"
+
+    local i
+    for i in "${!input_array[@]}"
+    do
+        if [[ "${input_array[i]}" == "$input_value" ]]
+        then
+            index_found=$i
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+_handle_args_find_array_index_by_value()
+{
+    _handle_args 'find_array_index_by_value' "$@"
+
+    ###
+    # Arguments - Singular values
+    input_value="${non_flagged_args[0]}"
+    non_flagged_args=("${non_flagged_args[@]:1}")
+    #
+    ###
+
+    ###
+    # Arguments - Arrays
+    local dynamic_array_prefix="input_array"
+    handle_input_arrays_dynamically "$dynamic_array_prefix" \
+                                    "${non_flagged_args[@]}"
+
+    get_dynamic_array "${dynamic_array_prefix}1"
+    input_array=("${dynamic_array[@]}")
+    #
+    ###
+}
